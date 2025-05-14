0623d41693d63c6e50544cb8303192d3f90d6d88
Sean MacAvaney <sean.macavaney@gmail.com>
2025-05-14 13:25:02 +0100

    test import

--- tests/test_pyterrier_serve.py
+++ tests/test_pyterrier_serve.py
@@ -1,5 +1,9 @@
 import unittest
 
 
-class TestPyterrier_serve(unittest.TestCase):
-    pass
+class TestPyterrierServe(unittest.TestCase):
+    def test_import(self):
+        try:
+            import pyterrier_serve
+        except ImportError as e:
+            self.fail(f"Import failed: {e}")
